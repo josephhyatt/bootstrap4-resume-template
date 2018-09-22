5050485d0920158a5634a054ff579486bf80b8d6
josephhyatt <josephnhyatt@gmail.com>
2018-09-22 15:06:56 -0700

    making it better

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 // Include the three packages we need
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
@@ -12,7 +12,7 @@ gulp.task('sass', function() {
 });
 
 // Move the javascript files into our /src/js folder
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     // places above code into "src/js"
     .pipe(gulp.dest("src/js"))
@@ -20,16 +20,16 @@ gulp.task('js', function() {
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
   // launch a localhost:3000
   browserSync.init({
-    server: "./src"  
+    server: "./src"
   });
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+
 // Any gulp task thats named 'default' 
 // allows us to simply jump into the command line
 // and run any command specified in the square brackets
-gulp.task('default', ['js','serve']);
-
+gulp.task('default', ['js', 'serve']);
